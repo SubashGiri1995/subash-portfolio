748d4b480b4b89c06cc4082abdbf3375f7429341
SubashGiri1995 <86644834+SubashGiri1995@users.noreply.github.com>
2022-07-15 17:32:32 +1000

    Updates

---
+++ precache-manifest.1fbec7eeafcc8fbbaf733101599b5e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa545774e59c37ee3738f9663ca2993",
+    "url": "/SubashGiri1995/subash-portfolio/index.html"
+  },
+  {
+    "revision": "b39e78652c2a074854ff",
+    "url": "/SubashGiri1995/subash-portfolio/static/css/main.d0acee29.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/SubashGiri1995/subash-portfolio/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/SubashGiri1995/subash-portfolio/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b39e78652c2a074854ff",
+    "url": "/SubashGiri1995/subash-portfolio/static/js/main.0a5aec5d.chunk.js"
+  },
+  {
+    "revision": "61fd778afecb80724ab3",
+    "url": "/SubashGiri1995/subash-portfolio/static/js/runtime-main.148d70ee.js"
+  },
+  {
+    "revision": "7948284f857a7f2430b9ecd2a4f9ff02",
+    "url": "/SubashGiri1995/subash-portfolio/static/media/Subash-photo.7948284f.jpeg"
+  }
+]);
